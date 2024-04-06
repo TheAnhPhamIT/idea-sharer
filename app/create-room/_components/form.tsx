1a4dc946902f4ca1d862837d3a6eec9779ff09fe
theanhphamit <theanhphamit@gmail.com>
2024-04-06 11:26:44 +0700

    switching to jwt strategy, adding id to session

---
+++ app/create-room/_components/form.tsx
@@ -0,0 +1,132 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { createRoomAction } from "@/app/create-room/actions";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+    name: z.string().min(1).max(50),
+    description: z.string().min(1).max(100),
+    githubRepo: z.string().min(1).max(100),
+    language: z.string().min(1).max(50),
+});
+
+export function CreateRoomForm() {
+    const router = useRouter();
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            description: "",
+            githubRepo: "",
+            language: "",
+        },
+    });
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        //TODO: invoke a server action to store the data in the database
+        await createRoomAction(values);
+        router.push("/");
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Name</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter room name"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                This is your public room name.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter description for your room"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                Please describe what you are be coding on, help
+                                the others have a general vision about your idea
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="githubRepo"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Github Repo</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter your Github repo link"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                Please put a link to the project you are working
+                                on
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="language"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Primary Programming Language</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Enter programming languages"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                List the primary programming languages you are
+                                working with
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 // import * as React from "react";
 // import { twMerge } from "tailwind-merge";
 
+import { db } from "@/db";
+
 export default async function Home() {
+    const rooms = await db.query.room.findMany();
+
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            {rooms.map((room) => (
+                <div key={room.name}>{room.name}</div>
+            ))}
+        </main>
     );
 }
 
